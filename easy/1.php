876f92b1e67f700db9bb7867857807e8cee6c57c
Evan Clements <me@evanclements.co>
2015-02-11 09:43:00 -0500

    Created 1.php; Added Docblock and Form.

---
+++ easy/1.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Challenge #1
+ * Difficulty: Easy
+ * URL: http://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/ 
+ *
+ * Create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+ * --> your name is (blank), you are (blank) years old, and your username is (blank)
+ * for extra credit, have the program log this information in a file to be accessed later.
+ * 
+ */
+
+?>
+<html>
+	<form>
+		<input type="text" name="name" />
+		<input type="number" name="age" />
+		<input type="text" name="username" />
+		<input type="submit" />
+	</form>
+</html>
+<?php
+
+?>
