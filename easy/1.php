569e1c58302e8cdcf522d455b8ab94b9207a7d58
Evan Clements <me@evanclements.co>
2015-02-17 10:44:56 -0500

    Added in DL link, and checked for existing file for user.

--- easy/1.php
+++ easy/1.php
@@ -16,10 +16,20 @@ if($_REQUEST['submitted'] === "yes" && $_POST <> null){
 	// Build file to hold person's info in. 
 	$userInfo = $_POST['username'].".json";
 	$fh = fopen($userInfo, 'w') or die("can't open file");
-	fwrite($fh, $request);
+	
+	// If file doesn't exist, create it.
+	if($fh){
+		fwrite($fh, $request);
+	};
+	
 	fclose($fh);
 	
-	print_r("Your name is ".$_POST['name'].", you are ".$_POST['age'].", and your Reddit username is <a href=\"http://www.reddit.com/u/".$_POST['username']."\">".$_POST['username']."</a>.");
+	print_r(
+		"Your name is ".$_POST['name']
+		.", you are ".$_POST['age'].
+		", and your Reddit username is <a href=\"http://www.reddit.com/u/".$_POST['username']."\">".$_POST['username']."</a>."
+		" Download your JSON file here: <a href=\"http://evanclements.co/projects/codekata/easy/".$userInfo."\" download=\"info.json\">here</a>."	
+	);
 }else{
 
 ?>
